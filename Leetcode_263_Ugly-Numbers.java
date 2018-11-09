c439c610465456c47ee46614b31d0fe5790ff00e
mrsurprise123 <44513517+mrsurprise123@users.noreply.github.com>
2018-11-09 11:06:06 -0500

    Add files via upload

---
+++ Leetcode_263_Ugly-Numbers.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isUgly(int num) {
+        while(num >= 2){
+            if(num % 2 == 0){
+                num /= 2;
+            }
+            else if(num % 3 == 0){
+                num /= 3;
+            }
+            else if(num % 5 == 0){
+                num /= 5;
+            }
+            else{
+                return false;
+            }
+        }
+        return num == 1;
+    }
+}
